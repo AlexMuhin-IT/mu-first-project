d86b3e6dfb35d38ccdf4ff281075ec4fe2b45028
alexMuhin <myx87@bk.ru>
2024-06-14 23:11:15 +0300

    add button in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <StyleBtn>Hello</StyleBtn>
     </div>
   );
 }
 
 export default App;
+
+
+const StyleBtn = styled.button`
+  border: none;
+  background-color: blue;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: bolt;
+`
