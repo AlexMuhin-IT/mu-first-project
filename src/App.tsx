c1950f9e9361fa7066e45ea1c52256c53d2c6208
alexMuhin <myx87@bk.ru>
2024-06-15 16:09:24 +0300

    add folder components and transfer fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components';
+import { Box, Link, StyleBtn, SuperButton } from './components/Button.styled';
 
 function App() {
   return (
     <div className="App">
       <Box>
+        <StyleBtn as={Link}>Link</StyleBtn>
         <StyleBtn as="a">Link</StyleBtn>
         <StyleBtn>Hello</StyleBtn>
         <SuperButton>Super Button</SuperButton>
@@ -17,22 +19,5 @@ function App() {
 export default App;
 
 
-const StyleBtn = styled.button`
-  border: none;
-  background-color: blue;
-  padding: 10px 20px;
-  color: #fff;
-  font-size: 2rem;
-  font-weight: bold;
-`
-const SuperButton = styled(StyleBtn)`
-  border-radius: 5px;
-  background-color: red;
-`
 
-const Box = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+
---
+++ src/components/Button.styled.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const StyleBtn = styled.button`
+  border: none;
+  background-color: blue;
+  padding: 10px 20px;
+  color: #fff;
+  font-size: 2rem;
+  font-weight: bold;
+  
+  &:hover{
+    color: red;
+    cursor: pointer;
+  }
+  &:last-child {
+    color: #3208eb
+  }
+
+`
+export const SuperButton = styled(StyleBtn)`
+  border-radius: 5px;
+  background-color: red;
+`
+export const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  button {
+  color: black;
+  
+  }
+`
+export const Link = styled.a`
+color: #4983c3;
+font-size: 2rem;
+font-weight: bold;
+background-color: #0ecf28;
+padding: 0;
+`
